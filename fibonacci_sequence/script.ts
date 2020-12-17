2f98ac36ea448ffd1d534bd7c23b5a3517140a93
Frank McNulty <f-mcnulty@hotmail.com>
2020-12-17 17:33:33 +0000

    fib

---
+++ fibonacci_sequence/script.ts
@@ -0,0 +1,11 @@
+// return n'th number of fibonacci sequence
+let displayOutput = document.querySelector(".output");
+
+const fibonacci = (n: number) => {
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+displayOutput.innerHTML = `Output: ${fibonacci(8)}`;
+
