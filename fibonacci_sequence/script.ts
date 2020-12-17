2fdd9b9daf83450c98c9f8fa2ee62462276f92dc
Frank McNulty <f-mcnulty@hotmail.com>
2020-12-17 17:33:39 +0000

    fib

--- fibonacci_sequence/script.ts
+++ fibonacci_sequence/script.ts
@@ -4,7 +4,7 @@ let displayOutput = document.querySelector(".output");
 const fibonacci = (n: number) => {
     if (n <= 2) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
-}
+} 
 
 
 displayOutput.innerHTML = `Output: ${fibonacci(8)}`;
